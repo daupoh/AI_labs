d1960ee7929596cd58135ad69b68d4c0f92d9db9
Стоялов Евгений <levievgen26@gmail.com>
2019-11-22 16:01:01 +0300

    continue with net class

--- CNet.cs
+++ CNet.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,53 @@ namespace wf_AI_lab1
 {
     class CNet
     {
+        int m_iCountOfVertex;
+        struct Rib
+        {
+            double fDistance;
+            double fFerment;            
+            public double Distance
+            {
+                get
+                {
+                    return fDistance;
+                }
+                set
+                {
+                    Assert.IsTrue(value > 0, "Значение расстояния между вершинами не может быть меньше или равно 0.");
+                    fDistance = value;
+                }
+            }
+            public double Ferment
+            {
+                get
+                {
+                    return fFerment;
+                }
+                set
+                {
+                    Assert.IsTrue(value >= 0, "Значение расстояния между вершинами не может быть меньше 0.");
+                    fFerment = value;
+                }
+            }
+            public Rib(double fDistance, double fFerment)
+            {
+                this.fDistance = fDistance;
+                this.fFerment = fFerment;
+            }
+        }
+        Rib[][] m_aGraph;
+
+        public int CountOfVertex
+        {
+            get
+            {
+                return m_iCountOfVertex;
+            }
+            private set
+            {
+
+            }
+        }
     }
 }
