f527f4fc2cbfb49e149be6126ff9af4b6124d886
Стоялов Евгений <levievgen26@gmail.com>
2019-11-24 23:53:55 +0300

    work on update pheromones

---
+++ CAnt.cs
@@ -0,0 +1,117 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wf_AI_lab1
+{
+    class CAnt
+    {
+        CNet m_rNet;
+        protected int[] m_aTabu;
+        protected int m_iStartVertex,m_iPosition;
+        protected Random m_rRandomiser;
+       public int StartPos
+        {
+            get
+            {
+                return m_iStartVertex;
+            }
+            set
+            {
+                Assert.IsTrue(value > 0 && value < m_rNet.CountOfVertex);
+                m_iStartVertex = value;                
+            }
+        }
+        public int[] Path
+        {
+            get
+            {
+                if (IsPathFinished())
+                {
+                    return m_aTabu;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+        public CAnt(CNet rNet)
+        {
+            Assert.IsTrue(rNet !=null);
+            m_rNet = rNet;
+            m_aTabu = new int[m_rNet.CountOfVertex];          
+            m_rRandomiser = new Random();
+            m_iStartVertex = 0;
+            m_iPosition = 0;
+            m_iStartVertex = (int)Math.Truncate(m_rRandomiser.NextDouble() * m_rNet.CountOfVertex);           
+        }
+        public void PrepareToRun()
+        {
+            for (int i = 0; i < m_rNet.CountOfVertex; i++)
+            {
+                m_aTabu[i] = -1;
+            }            
+            m_aTabu[0] = StartPos;
+            m_iPosition++;
+        }
+
+        public void Run(double fAlpha, double fBeta)
+        {           
+            while (m_iPosition < m_aTabu.Length)
+            {
+                int iCurrentPos = m_aTabu[m_iPosition-1], iNextPos = -1;
+                double[] aProbability = new double[m_rNet.CountOfVertex],
+                       aPheromones = m_rNet.GetPheromoneDestinations(iCurrentPos),
+                       aAttractives = m_rNet.GetAttractiveDestinations(iCurrentPos);
+                double fSumAttractivePheromones = 0, fMaxProbability = 0;                
+                for (int i = 0; i < m_rNet.CountOfVertex; i++)
+                {
+                    if (NotTabu(i))
+                    {
+                        fSumAttractivePheromones += Math.Pow(aPheromones[i], fAlpha) * Math.Pow(aAttractives[i], fBeta);
+                    }
+                }                
+                for (int i = 0; i < m_rNet.CountOfVertex; i++)
+                {
+                    if (NotTabu(i))
+                    {
+                        aProbability[i] = Math.Pow(aPheromones[i], fAlpha) * Math.Pow(aAttractives[i], fBeta) / fSumAttractivePheromones;
+                        if (aProbability[i] > fMaxProbability)
+                        {
+                            fMaxProbability = aProbability[i];
+                            iNextPos = i;
+                        }
+                    }
+                }
+                m_aTabu[m_iPosition++] = iNextPos;
+            }
+        }
+        private bool NotTabu(int iPos)
+        {
+            bool bIsIt = true;
+            for (int i = 0; i <= m_iPosition; i++)
+            {
+                if (m_aTabu[i]==iPos)
+                {
+                    bIsIt = false;
+                    break;
+                }
+            }
+            return bIsIt;
+        }
+        private bool IsPathFinished()
+        {
+            bool bIsIt = true;
+            int i = 0;
+            while (bIsIt && i < m_aTabu.Length)
+            {
+                bIsIt = bIsIt && m_aTabu[i++] != -1;
+            }
+            return bIsIt;
+        }
+    }
+}
--- CNet.cs
+++ CNet.cs
@@ -10,10 +10,11 @@ namespace wf_AI_lab1
     class CNet
     {
         int m_iCountOfVertex;
-        struct Rib
+        double m_fRibAttraction, m_fAlpha, m_fBeta;
+        public struct Rib
         {
             double fDistance;
-            double fFerment;            
+            double fPheromone;
             public double Distance
             {
                 get
@@ -26,26 +27,26 @@ namespace wf_AI_lab1
                     fDistance = value;
                 }
             }
-            public double Ferment
+            public double Pheromone
             {
                 get
                 {
-                    return fFerment;
+                    return fPheromone;
                 }
                 set
                 {
                     Assert.IsTrue(value >= 0, "Значение расстояния между вершинами не может быть меньше 0.");
-                    fFerment = value;
+                    fPheromone = value;
                 }
             }
             public Rib(double fDistance, double fFerment)
             {
                 this.fDistance = fDistance;
-                this.fFerment = fFerment;
+                this.fPheromone = fFerment;
             }
         }
-        Rib[][] m_aGraph;
 
+        Rib[][] m_aGraph;
         public int CountOfVertex
         {
             get
@@ -54,7 +55,123 @@ namespace wf_AI_lab1
             }
             private set
             {
+                Assert.IsTrue(value > 1);
+                m_iCountOfVertex = value;
+            }
+        }
+        public double PheromonePower
+        {
+            get
+            {
+                return m_fAlpha;
+            }
+            set
+            {
+                Assert.IsTrue(value > 0);
+                m_fAlpha = value;
+            }
+        }
+        public double DistancePower
+        {
+            get
+            {
+                return m_fBeta;
+            }
+            set
+            {
+                Assert.IsTrue(value > 0);
+                m_fBeta= value;
+            }
+        }
+        public double Attraction
+        {
+            get
+            {
+                return m_fRibAttraction;
+            }
+            set
+            {
+                Assert.IsTrue(value >= 1);
+                m_fRibAttraction = value;
+            }
+        }
 
+        public CNet(int iCountOfVertex)
+        {
+            CountOfVertex = iCountOfVertex;
+            m_fRibAttraction = 1;
+            m_fAlpha = 1;
+            m_fBeta = 1;
+            m_aGraph = new Rib[CountOfVertex][];
+            for (int i = 0; i < CountOfVertex; i++)
+            {
+                m_aGraph[i] = new Rib[CountOfVertex];
+                for (int j = 0; j < CountOfVertex; j++)
+                {
+                    m_aGraph[i][j] = new Rib(1, 0);
+                }
+            }
+        }
+        public double[] GetAttractiveDestinations(int iFromPosition)
+        {
+            Assert.IsTrue(iFromPosition >= 0 && iFromPosition < CountOfVertex);
+            double[] aAttractiveDestinations = new double[CountOfVertex];
+            for (int i = 0; i < CountOfVertex; i++)
+            {
+                aAttractiveDestinations[i] = Attraction / m_aGraph[iFromPosition][i].Distance;
+            }
+            return aAttractiveDestinations;
+        }
+        public double[] GetPheromoneDestinations (int iFromPosition)
+        {
+            Assert.IsTrue(iFromPosition >= 0 && iFromPosition < CountOfVertex);
+            double[] aPheromoneDestinations = new double[CountOfVertex];
+            for (int i = 0; i < CountOfVertex; i++)
+            {
+                aPheromoneDestinations[i] = m_aGraph[iFromPosition][i].Pheromone;
+            }
+            return aPheromoneDestinations;
+        }
+        
+        public void UpdatePheromones (int[] aPath)
+        {
+            double fSumOfPheromones = Attraction / GetPathLength(aPath);
+        }
+       
+        public void SetRibs(double[][] aDistanceMatrix)
+        {
+            Assert.IsTrue(aDistanceMatrix.Length == CountOfVertex);
+            foreach (double[] aRow in aDistanceMatrix)
+            {
+                Assert.IsTrue(aRow.Length == CountOfVertex);
+            }
+            for (int i = 0; i < CountOfVertex; i++)
+            {
+                for (int j = 0; j < CountOfVertex; j++)
+                {
+                    m_aGraph[i][j].Distance = aDistanceMatrix[i][j];
+                    m_aGraph[i][j].Pheromone = 0;
+                }
+            }
+        }
+        public double GetPathLength(int[] aPath)
+        {
+            CheckPath(aPath);
+            double fLength = 0;
+            int iStart = aPath[0];
+            for (int i = 1; i < aPath.Length; i++)
+            {
+                fLength += m_aGraph[iStart][aPath[i]].Distance;
+                iStart = aPath[i];
+            }
+            return fLength;
+        }
+      
+        private void CheckPath(int[] aPath)
+        {
+            for (int i = 0; i < aPath.Length; i++)
+            {
+                Assert.IsTrue(aPath[i] >= 0 && aPath[i] < CountOfVertex);
             }
         }
     }
